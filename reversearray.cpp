629e4b6e46f33f3a7f2ffb61f750d397fc29d88c
Ayush-Pandey0 <ayushpandeyaa22@gmail.com>
2025-05-13 23:38:39 +0530

    Reverse array using two pointer approach

---
+++ reversearray.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        // reverse(arr.begin(),arr.end());
+
+
+        int i=0;
+        int j=arr.size()-1;
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+};
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        ob.reverseArray(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
