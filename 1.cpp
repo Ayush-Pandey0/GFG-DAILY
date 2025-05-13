69785db4fe7f6cd4de29d23f6d9250f661d64d53
Ayush-Pandey0 <ayushpandeyaa22@gmail.com>
2025-05-13 22:54:46 +0530

    second largest elemnt

--- 1.cpp
+++ 1.cpp
@@ -1,66 +1,42 @@
-#include <iostream>
-#include <vector>
-#include <string>
+
+#include <bits/stdc++.h>
+
 using namespace std;
 
-// Function to calculate factorial
-int factorial(int n) {
-    if (n <= 1) return 1;
-    return n * factorial(n - 1);
-}
 
-// Function to check if a number is prime
-bool isPrime(int num) {
-    if (num <= 1) return false;
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) return false;
+
+class Solution {
+  public:
+    int getSecondLargest(vector<int> &arr) {
+        // code here
+        int size=arr.size();
+        sort(arr.begin(),arr.end());
+        if(size<2) return -1;
+        int mx=arr[size-1];
+        for(int i=size-2;i>=0;i--){
+            if(mx!=arr[i]) return arr[i];
+        }
+        return -1;
     }
-    return true;
-}
+};
 
 int main() {
-    // Basic input/output
-    cout << "Welcome to C++ Programming Practice!" << endl;
-    
-    // Variables and user input
-    int number;
-    cout << "Enter a number: ";
-    cin >> number;
-    
-    // Demonstrate factorial calculation
-    cout << "Factorial of " << number << " is: " << factorial(number) << endl;
-    
-    // Demonstrate prime number check
-    if (isPrime(number)) {
-        cout << number << " is a prime number" << endl;
-    } else {
-        cout << number << " is not a prime number" << endl;
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.getSecondLargest(arr);
+        cout << ans << endl;
+        cout << "~" << endl;
     }
-    
-    // Vector demonstration
-    vector<int> numbers = {1, 2, 3, 4, 5};
-    cout << "\nVector elements: ";
-    for (int num : numbers) {
-        cout << num << " ";
-    }
-    
-    // Calculate sum of vector elements
-    int sum = 0;
-    for (int num : numbers) {
-        sum += num;
-    }
-    cout << "\nSum of vector elements: " << sum << endl;
-    
-    // String operations
-    string message = "C++ Programming";
-    cout << "\nString operations:" << endl;
-    cout << "Original string: " << message << endl;
-    cout << "Length: " << message.length() << endl;
-    
-    // Demonstrate string concatenation
-    string additional = " is fun!";
-    message += additional;
-    cout << "After concatenation: " << message << endl;
-    
     return 0;
 }
